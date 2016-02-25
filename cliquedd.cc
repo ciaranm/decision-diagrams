f181cd506ccd0a3b492388dc6c07b99b0a4fa84c
Ciaran McCreesh <c.mccreesh.1@research.gla.ac.uk>
2016-02-25 20:35:40 +0000

    Options

--- cliquedd.cc
+++ cliquedd.cc
@@ -9,6 +9,8 @@
 #include <atomic>
 #include <chrono>
 #include <mutex>
+#include <thread>
+#include <condition_variable>
 #include <cstdlib>
 
 #include <boost/dynamic_bitset.hpp>
@@ -24,12 +26,18 @@ using std::vector;
 using std::atomic;
 using std::mutex;
 using std::unique_lock;
+using std::thread;
+using std::condition_variable;
+using std::cv_status;
+using std::string;
 
 using std::cout;
 using std::endl;
+using std::boolalpha;
 
 using std::chrono::duration_cast;
 using std::chrono::milliseconds;
+using std::chrono::seconds;
 using std::chrono::steady_clock;
 using std::chrono::time_point;
 
@@ -41,13 +49,11 @@ struct Stats
     atomic<unsigned long long> nodes_reused{ 0 };
     atomic<unsigned long long> bdds_created{ 0 };
     atomic<unsigned long long> bdds_pruned{ 0 };
-
-    time_point<steady_clock> start_time = steady_clock::now();
 };
 
 struct Node
 {
-    unsigned long long score;           // number of accepted vertices so far (i.e. path length)
+    unsigned long long score; // number of accepted vertices so far (i.e. path length)
     dynamic_bitset<> clique;  // vertices accepted so far (i.e. one path of best length)
     dynamic_bitset<> state;   // vertices which remain to be selectable
 };
@@ -59,9 +65,9 @@ struct Level
     /**
      * Add node to level, updating stats, merging it if it is a duplicate.
      */
-    void add_node(Node && node, Stats & stats)
+    void add_node(Node && node, Stats & stats, const bool dominate)
     {
-        for (auto & d : nodes)
+        for (auto & d : nodes) {
             if (d.state == node.state) {
                 if (node.score > d.score) {
                     d.score = node.score;
@@ -70,6 +76,16 @@ struct Level
                 ++stats.nodes_reused;
                 return;
             }
+            else if (dominate && d.score <= node.score && d.state.is_subset_of(node.state)) {
+                d = node;
+                ++stats.nodes_reused;
+                return;
+            }
+            else if (dominate && node.score <= d.score && node.state.is_subset_of(d.state)) {
+                ++stats.nodes_reused;
+                return;
+            }
+        }
 
         nodes.push_back(move(node));
         ++stats.nodes_created;
@@ -116,13 +132,13 @@ struct Incumbent
     }
 };
 
-void solve(const Graph & graph, BDD & bdd, Incumbent & incumbent, Stats & stats);
+void solve(const Graph & graph, BDD & bdd, Incumbent & incumbent, Stats & stats, atomic<bool> & abort, const bool dominate);
 
 /**
  * Select a vertex to branch on, from level. May return -1, if there are no
  * vertices to branch on in the restricted case.
  */
-int select_branch_vertex(const Graph & graph, const Level & level)
+int select_branch_vertex_in_most_states(const Graph & graph, const Level & level)
 {
     vector<int> counts(graph.size());
     for (auto & n : level.nodes)
@@ -141,7 +157,7 @@ int select_branch_vertex(const Graph & graph, const Level & level)
 /**
  * Build level of bdd by branching on branch_vertex.
  */
-void build_level(const Graph & graph, BDD & bdd, int level, int branch_vertex, Stats & stats)
+void build_level(const Graph & graph, BDD & bdd, int level, int branch_vertex, Stats & stats, const bool dominate)
 {
     bdd.levels[level].nodes.reserve(bdd.levels[level - 1].nodes.size() * 2);
 
@@ -154,13 +170,13 @@ void build_level(const Graph & graph, BDD & bdd, int level, int branch_vertex, S
             accept.score += graph.weight(branch_vertex);
             accept.clique.set(branch_vertex);
             accept.state &= graph.neighbourhood(branch_vertex);
-            bdd.levels[level].add_node(move(accept), stats);
+            bdd.levels[level].add_node(move(accept), stats, dominate);
         }
 
         // The new level of the BDD always gets a reject node.
         Node reject = n;
         reject.state.set(branch_vertex, false);
-        bdd.levels[level].add_node(move(reject), stats);
+        bdd.levels[level].add_node(move(reject), stats, dominate);
     }
 }
 
@@ -168,15 +184,18 @@ void build_level(const Graph & graph, BDD & bdd, int level, int branch_vertex, S
  * Solve a relaxed form of the BDD, to get an upper bound on its actual best
  * value.
  */
-unsigned solve_relaxed(const Graph & graph, BDD & bdd, Stats & stats)
+unsigned solve_relaxed(const Graph & graph, BDD & bdd, Stats & stats, atomic<bool> & abort, const bool dominate)
 {
+    if (abort.load())
+        return 0;
+
     // Build up each level...
     for (unsigned level = 1 ; level <= bdd.height ; ++level) {
         --bdd.n_unassigned;
 
         // Pick a branch vertex, and build the next level.
-        int branch_vertex = select_branch_vertex(graph, bdd.levels[level - 1]);
-        build_level(graph, bdd, level, branch_vertex, stats);
+        int branch_vertex = select_branch_vertex_in_most_states(graph, bdd.levels[level - 1]);
+        build_level(graph, bdd, level, branch_vertex, stats, dominate);
 
         // Is our newly created level too wide?
         auto & level_nodes = bdd.levels[level].nodes;
@@ -209,8 +228,11 @@ unsigned solve_relaxed(const Graph & graph, BDD & bdd, Stats & stats)
 /**
  * Solve a restricted form of the BDD, in the hopes of getting a good incumbent quickly.
  */
-void solve_restricted(const Graph & graph, BDD & bdd, Incumbent & incumbent, Stats & stats)
+void solve_restricted(const Graph & graph, BDD & bdd, Incumbent & incumbent, Stats & stats, atomic<bool> & abort, const bool dominate)
 {
+    if (abort.load())
+        return;
+
     int last_level = 0;
 
     // Build up each level...
@@ -218,11 +240,11 @@ void solve_restricted(const Graph & graph, BDD & bdd, Incumbent & incumbent, Sta
         --bdd.n_unassigned;
 
         // Pick a branch vertex, and build the next level.
-        int branch_vertex = select_branch_vertex(graph, bdd.levels[level - 1]);
+        int branch_vertex = select_branch_vertex_in_most_states(graph, bdd.levels[level - 1]);
         if (-1 == branch_vertex)
             break;
 
-        build_level(graph, bdd, level, branch_vertex, stats);
+        build_level(graph, bdd, level, branch_vertex, stats, dominate);
         last_level = level;
 
         // Is our newly created level too wide?
@@ -244,13 +266,17 @@ void solve_restricted(const Graph & graph, BDD & bdd, Incumbent & incumbent, Sta
 /**
  * Given a node in a BDD, solve it exactly by creating a new BDD.
  */
-void solve_by_branching(const Graph & graph, const BDD & bdd, const Node & n, Incumbent & incumbent, Stats & stats)
+void solve_by_branching(const Graph & graph, const BDD & bdd, const Node & n, Incumbent & incumbent, Stats & stats, atomic<bool> & abort,
+        const bool dominate)
 {
+    if (abort.load())
+        return;
+
     BDD relaxed_bdd(n.state.count(), bdd.n_unassigned);
 
     // Start by getting a relaxed bound, to see if it's worth continuing.
     relaxed_bdd.levels[0] = { Level{ { n } } };
-    unsigned bound = solve_relaxed(graph, relaxed_bdd, stats);
+    unsigned bound = solve_relaxed(graph, relaxed_bdd, stats, abort, dominate);
 
     // Are we any good?
     if (bound > incumbent.value) {
@@ -259,7 +285,7 @@ void solve_by_branching(const Graph & graph, const BDD & bdd, const Node & n, In
         BDD child_bdd(n.state.count(), bdd.n_unassigned);
         child_bdd.levels[0] = { Level{ { n } } };
 
-        solve(graph, child_bdd, incumbent, stats);
+        solve(graph, child_bdd, incumbent, stats, abort, dominate);
     }
     else {
         ++stats.bdds_pruned;
@@ -269,19 +295,23 @@ void solve_by_branching(const Graph & graph, const BDD & bdd, const Node & n, In
 /**
  * Solve a BDD.
  */
-void solve(const Graph & graph, BDD & bdd, Incumbent & incumbent, Stats & stats)
+void solve(const Graph & graph, BDD & bdd, Incumbent & incumbent, Stats & stats, atomic<bool> & abort,
+        const bool dominate)
 {
+    if (abort.load())
+        return;
+
     // Start by building a restricted BDD, just to get a good incumbent quickly.
     BDD restricted_bdd = bdd;
-    solve_restricted(graph, restricted_bdd, incumbent, stats);
+    solve_restricted(graph, restricted_bdd, incumbent, stats, abort, dominate);
 
     // Build up each level...
     for (unsigned level = 1 ; level <= bdd.height ; ++level) {
         --bdd.n_unassigned;
 
         // Pick a branch vertex, and build the next level.
-        int branch_vertex = select_branch_vertex(graph, bdd.levels[level - 1]);
-        build_level(graph, bdd, level, branch_vertex, stats);
+        int branch_vertex = select_branch_vertex_in_most_states(graph, bdd.levels[level - 1]);
+        build_level(graph, bdd, level, branch_vertex, stats, dominate);
 
         // Is our newly created level too wide?
         if (bdd.levels[level].nodes.size() > bdd.n_unassigned + 1) {
@@ -318,26 +348,66 @@ BDD create_root_bdd(const Graph & graph, Stats & stats)
 
 int main(int argc, char * argv[])
 {
-    if (argc != 2) {
-        cout << "Usage: " << argv[0] << " file.clq" << endl;
+    if (argc != 2 && (argc != 3 || string(argv[1]) != "-d")) {
+        cout << "Usage: " << argv[0] << " [ -d ] file.clq" << endl;
         return EXIT_FAILURE;
     }
 
-    Graph graph = read_dimacs(argv[1]);
+    int timeout = 14400;
+    bool aborted = false;
+
+    Graph graph = read_dimacs(argv[argc - 1]);
+
+    thread timeout_thread;
+    mutex timeout_mutex;
+    condition_variable timeout_cv;
+    atomic<bool> abort{ false };
+    if (0 != timeout) {
+        timeout_thread = thread([&] {
+                auto abort_time = steady_clock::now() + seconds(timeout);
+                {
+                    /* Sleep until either we've reached the time limit,
+                     * or we've finished all the work. */
+                    unique_lock<mutex> guard(timeout_mutex);
+                    while (! abort.load()) {
+                        if (cv_status::timeout == timeout_cv.wait_until(guard, abort_time)) {
+                            /* We've woken up, and it's due to a timeout. */
+                            aborted = true;
+                            break;
+                        }
+                    }
+                }
+                abort.store(true);
+                });
+    }
+
+    /* Start the clock */
+    auto start_time = steady_clock::now();
 
     Stats stats;
     Incumbent incumbent;
 
     BDD bdd = create_root_bdd(graph, stats);
-    solve(graph, bdd, incumbent, stats);
+    solve(graph, bdd, incumbent, stats, abort, argc == 3);
 
+    /* Clean up the timeout thread */
+    if (timeout_thread.joinable()) {
+        {
+            unique_lock<mutex> guard(timeout_mutex);
+            abort.store(true);
+            timeout_cv.notify_all();
+        }
+        timeout_thread.join();
+    }
+
+    cout << "timeout = " << boolalpha << aborted << endl;
     cout << "solution =";
     for (auto i = incumbent.clique.find_first() ; i != dynamic_bitset<>::npos ; i = incumbent.clique.find_next(i))
         cout << " " << i + 1; // dimacs files are 1-indexed
     cout << endl;
 
     cout << "size = " << incumbent.value << endl;
-    cout << "runtime = " << duration_cast<milliseconds>(steady_clock::now() - stats.start_time).count() << "ms" << endl;
+    cout << "runtime = " << duration_cast<milliseconds>(steady_clock::now() - start_time).count() << "ms" << endl;
     cout << "nodes = " << stats.nodes_created << " + " << stats.nodes_reused << endl;
     cout << "branches = " << stats.bdds_created << " + " << stats.bdds_pruned << endl;
 
